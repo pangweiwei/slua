bd7531ebc7f6bd0cb1ba4658528adf4e929554f5
pangweiwei <sineysan@yeah.net>
2015-02-08 16:26:10 +0800

    add hint on gui for performance_test

--- Assets/example/Perf.cs
+++ Assets/example/Perf.cs
@@ -7,33 +7,46 @@ public class Perf : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	    l = new LuaSvr("perf");
+
+		Application.RegisterLogCallback(this.log);
 	}
 
+	string logText="";
+	void log(string cond,string trace,LogType lt) {
+		logText+=cond;
+		logText+="\n";
+	}
 
     void OnGUI()
     {
+
         if (GUI.Button(new Rect(10, 10, 120, 50), "Test1"))
         {
+			logText="";
             l.luaState.getFunction("test1").call();
         }
 
         if (GUI.Button(new Rect(10, 100, 120, 50), "Test2"))
         {
+			logText="";
             l.luaState.getFunction("test2").call();
         }
 
         if (GUI.Button(new Rect(10, 200, 120, 50), "Test3"))
         {
+			logText="";
             l.luaState.getFunction("test3").call();
         }
 
         if (GUI.Button(new Rect(10, 300, 120, 50), "Test4"))
         {
+			logText="";
             l.luaState.getFunction("test4").call();
         }
 
         if (GUI.Button(new Rect(200, 10, 120, 50), "Test5"))
         {
+			logText="";
             l.luaState.getFunction("test5").call();
         }
 
@@ -41,5 +54,7 @@ public class Perf : MonoBehaviour {
         {
             Application.OpenURL("http://www.sineysoft.com/post/164");
         }
+
+		GUI.Label(new Rect(200,200,300,50),logText);
     }
 }
