75f2eceb9cdc43437e70680995af23ffddcac739
pangweiwei <sineysan@163.com>
2015-03-27 17:52:25 +0800

    fix code for lua5.3

--- Assets/Slua/slua_src/slua.c
+++ Assets/Slua/slua_src/slua.c
@@ -1,4 +1,4 @@
-/* 
+/*
 * This file is based off of MonoLuaInterface's wrapping: https://github.com/stevedonovan/MonoLuaInterface
 */
 
@@ -12,34 +12,34 @@
 #ifndef _WIN32
 #define __stdcall
 #endif
-typedef int (__stdcall *lua_stdcallCFunction) (lua_State *L);
+typedef int(__stdcall *lua_stdcallCFunction) (lua_State *L);
 
-static int stdcall_closure(lua_State *L) 
+static int stdcall_closure(lua_State *L)
 {
-    lua_stdcallCFunction fn = (lua_stdcallCFunction)lua_touserdata(L, lua_upvalueindex(1));
-    int r = fn(L);
-    return r;
+	lua_stdcallCFunction fn = (lua_stdcallCFunction)lua_touserdata(L, lua_upvalueindex(1));
+	int r = fn(L);
+	return r;
 }
 
 
 LUA_API void lua_pushstdcallcfunction(lua_State *L, lua_stdcallCFunction fn)
 {
-    lua_pushlightuserdata(L, fn);
-    lua_pushcclosure(L, stdcall_closure, 1);
+	lua_pushlightuserdata(L, fn);
+	lua_pushcclosure(L, stdcall_closure, 1);
 }
 
 
 LUA_API void luaS_newuserdata(lua_State *L, int val)
 {
-  int* pointer=(int*)lua_newuserdata(L,sizeof(int));
-  *pointer=val;
+	int* pointer = (int*)lua_newuserdata(L, sizeof(int));
+	*pointer = val;
 }
 
-LUA_API int luaS_rawnetobj(lua_State *L,int index) 
+LUA_API int luaS_rawnetobj(lua_State *L, int index)
 {
-  int *udata=lua_touserdata(L,index);
-  if(udata!=NULL) return *udata;
-  return -1;
+	int *udata = lua_touserdata(L, index);
+	if (udata != NULL) return *udata;
+	return -1;
 }
 
 // The MIT License (MIT)
@@ -93,7 +93,7 @@ static int s_closure(lua_State *L)
 }
 
 
-LUA_API void luaS_pushcclosure(lua_State *L, lua_CFunction func,int n) {
+LUA_API void luaS_pushcclosure(lua_State *L, lua_CFunction func, int n) {
 	if (func != NULL) {
 		lua_pushlightuserdata(L, func);
 		lua_pushcclosure(L, s_closure, n + 1);
@@ -101,10 +101,10 @@ LUA_API void luaS_pushcclosure(lua_State *L, lua_CFunction func,int n) {
 }
 
 LUA_API const char* luaS_tolstring32(lua_State *L, int index, int* len) {
-    size_t l;
-    const char* ret = lua_tolstring(L,index, &l);
-    *len = (int)l;
-    return ret;
+	size_t l;
+	const char* ret = lua_tolstring(L, index, &l);
+	*len = (int)l;
+	return ret;
 }
 
 static lua_stdcallCFunction panicf = NULL;
@@ -120,17 +120,17 @@ LUA_API void luaS_atpanic(lua_State *L, lua_stdcallCFunction f) {
 }
 
 #if LUA_VERSION_NUM>=503
-static int k (lua_State *L, int status, lua_KContext ctx) {
+static int k(lua_State *L, int status, lua_KContext ctx) {
 	return status;
 }
 
-LUA_API int luaS_yield(lua_State *L,int nrets) {
-	int ret = lua_yieldk(L,nrets,0,k);
+LUA_API int luaS_yield(lua_State *L, int nrets) {
+	int ret = lua_yieldk(L, nrets, 0, k);
 	return ret;
 }
 
-LUA_API int luaS_pcall(lua_State *L,int nargs,int nresults,int err) {
-	return k(L,lua_pcallk(L,nargs,nresults,err,0,k),0);
+LUA_API int luaS_pcall(lua_State *L, int nargs, int nresults, int err) {
+	return k(L, lua_pcallk(L, nargs, nresults, err, 0, k), 0);
 }
 #endif
 
@@ -144,20 +144,25 @@ static void getmetatable(lua_State *L, const char* key) {
 	snprintf(ns, 256, "UnityEngine.%s.Instance", key);
 #endif
 
-	lua_getfield(L,LUA_REGISTRYINDEX, ns);
+	lua_getfield(L, LUA_REGISTRYINDEX, ns);
 }
 
 static void setmetatable(lua_State *L, int p, int what) {
-	
+
 	int ref;
-	
+#if LUA_VERSION_NUM>=503
+	lua_pushglobaltable(L);
+	lua_rawgeti(L,-1,what);
+	lua_remove(L, -2);
+#else
 	lua_rawgeti(L, LUA_GLOBALSINDEX, what);
+#endif
 	if (!lua_isnil(L, -1)) {
 		ref = (int)lua_tointeger(L, -1);
 		lua_pop(L, 1);
 		if (ref != LUA_REFNIL)
 		{
-			lua_getref(L, ref);
+			lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
 		}
 	}
 	else {
@@ -180,8 +185,16 @@ static void setmetatable(lua_State *L, int p, int what) {
 
 		lua_pushvalue(L, -1);
 		ref = luaL_ref(L, LUA_REGISTRYINDEX);
+		
+#if LUA_VERSION_NUM>=503
+		lua_pushglobaltable(L);
+		lua_pushinteger(L, ref);
+		lua_rawseti(L, -2, what);
+		lua_pop(L,1);
+#else
 		lua_pushinteger(L, ref);
 		lua_rawseti(L, LUA_GLOBALSINDEX, what);
+#endif
 	}
 
 	lua_setmetatable(L, p);
@@ -206,13 +219,13 @@ LUA_API int luaS_checkluatype(lua_State *L, int p, const char *t) {
 		lua_settop(L, top);
 		return 0;
 	}
-	b=lua_tostring(L, -1);
+	b = lua_tostring(L, -1);
 	lua_settop(L, top);
-	return strcmp(t, b)==0;
+	return strcmp(t, b) == 0;
 }
 
 
-LUA_API void luaS_checkVector4(lua_State *L, int p, float* x, float *y, float *z,float *w) {
+LUA_API void luaS_checkVector4(lua_State *L, int p, float* x, float *y, float *z, float *w) {
 	luaL_checktype(L, p, LUA_TTABLE);
 	lua_getfield(L, p, "x");
 	*x = (float)lua_tonumber(L, -1);
@@ -225,7 +238,7 @@ LUA_API void luaS_checkVector4(lua_State *L, int p, float* x, float *y, float *z
 	lua_pop(L, 4);
 }
 
-LUA_API void luaS_pushVector4(lua_State *L, float x, float y, float z,float w) {
+LUA_API void luaS_pushVector4(lua_State *L, float x, float y, float z, float w) {
 	lua_newtable(L);
 	lua_pushnumber(L, x);
 	lua_setfield(L, -2, "x");
@@ -295,7 +308,7 @@ LUA_API void luaS_checkQuaternion(lua_State *L, int p, float* x, float *y, float
 	lua_pop(L, 4);
 }
 
-LUA_API void luaS_pushQuaternion(lua_State *L, float x, float y, float z,float w) {
+LUA_API void luaS_pushQuaternion(lua_State *L, float x, float y, float z, float w) {
 	lua_newtable(L);
 	lua_pushnumber(L, x);
 	lua_setfield(L, -2, "x");
@@ -320,7 +333,7 @@ static void setelement(lua_State* L, int p, float v, const char* key) {
 }
 
 
-LUA_API void luaS_setData(lua_State *L,int p, float x, float y, float z, float w) {
+LUA_API void luaS_setData(lua_State *L, int p, float x, float y, float z, float w) {
 	setelement(L, p, x, "x");
 	setelement(L, p, y, "y");
 	setelement(L, p, z, "z");
@@ -328,13 +341,13 @@ LUA_API void luaS_setData(lua_State *L,int p, float x, float y, float z, float w
 }
 
 static void cacheud(lua_State *l, int index, int cref) {
-	lua_getref(l, cref);
+	lua_rawgeti(l,LUA_REGISTRYINDEX,cref);
 	lua_pushvalue(l, -2);
 	lua_rawseti(l, -2, index);
 	lua_pop(l, 1);
 }
 
-LUA_API void luaS_pushobject(lua_State *l,int index,const char* t,int gco,int cref) {
+LUA_API void luaS_pushobject(lua_State *l, int index, const char* t, int gco, int cref) {
 	luaS_newuserdata(l, index);
 	if (gco) cacheud(l, index, cref);
 
@@ -349,8 +362,8 @@ LUA_API void luaS_pushobject(lua_State *l,int index,const char* t,int gco,int cr
 	lua_setmetatable(l, -2);
 }
 
-LUA_API int luaS_getcacheud(lua_State *l,int index,int cref) {
-	lua_getref(l, cref);
+LUA_API int luaS_getcacheud(lua_State *l, int index, int cref) {
+	lua_rawgeti(l, LUA_REGISTRYINDEX, cref);
 	lua_rawgeti(l, -1, index);
 	if (!lua_isnil(l, -1))
 	{
