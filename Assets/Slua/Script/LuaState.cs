bea0855464f4b5f0b68c82c465fe82a62ca711a0
pangweiwei <sineysan@163.com>
2015-05-07 17:26:42 +0800

    add luaS_openextlibs for 3rd lualibs

--- Assets/Slua/Script/LuaState.cs
+++ Assets/Slua/Script/LuaState.cs
@@ -485,6 +485,12 @@ namespace SLua
 			refQueue = new Queue<UnrefPair>();
 
 			LuaDLL.luaL_openlibs(L);
+			try
+			{
+				LuaDLL.luaS_openextlibs(L);
+			} catch(Exception) {
+				// do nothing
+			}
 
 			ObjectCache.make(L);
 
