bb13c61f4a3cdf091b7f9acb11c3892c9edeac5b
Yongkang Chen <lx1988cyk@gmail.com>
2015-05-13 16:27:24 +0800

    fix crash, same as #26.

--- Assets/Slua/Script/Timer.cs
+++ Assets/Slua/Script/Timer.cs
@@ -261,12 +261,13 @@ namespace SLua
 					ua = (Action<int>)ld.d;
 				else
 				{
+					IntPtr ml = LuaState.get(l).L;
 					ua = (int id) =>
 					{
-						int error = pushTry(l);
-						pushValue(l, id);
+						int error = pushTry(ml);
+						pushValue(ml, id);
 						ld.call(1, error);
-						LuaDLL.lua_settop(l, error - 1);
+						LuaDLL.lua_settop(ml, error - 1);
 					};
 				}
 				ld.d = ua;
@@ -286,13 +287,14 @@ namespace SLua
 					ua = (Func<int, bool>)ld.d;
 				else
 				{
+					IntPtr ml = LuaState.get(l).L;
 					ua = (int id) =>
 					{
-						int error = pushTry(l);
-						pushValue(l, id);
+						int error = pushTry(ml);
+						pushValue(ml, id);
 						ld.call(1, error);
-						bool ret = LuaDLL.lua_toboolean(l, -1);
-						LuaDLL.lua_settop(l, error - 1);
+						bool ret = LuaDLL.lua_toboolean(ml, -1);
+						LuaDLL.lua_settop(ml, error - 1);
 						return ret;
 					};
 				}
