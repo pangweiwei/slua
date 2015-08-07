b602886e5b2b91bcc3759ddb44ad255c35b9aa29
pangweiwei <sineysan@yeah.net>
2015-08-07 11:03:19 +0800

    add build script

---
+++ build/make_ios.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cp ../Assets/slua_src/slua.c luajit-2.1.0/src/
+cd luajit-2.1.0
+LIPO="xcrun -sdk iphoneos lipo"
+STRIP="xcrun -sdk iphoneos strip"
+
+IXCODE=`xcode-select -print-path`
+ISDK=$IXCODE/Platforms/iPhoneOS.platform/Developer
+ISDKVER=iPhoneOS8.4.sdk
+ISDKP=$IXCODE/usr/bin/
+
+if [ ! -e $ISDKP/ar ]; then 
+  sudo cp $ISDK/usr/bin/ar $ISDKP
+fi
+
+if [ ! -e $ISDKP/ranlib ]; then
+  sudo cp $ISDK/usr/bin/ranlib $ISDKP
+fi
+
+if [ ! -e $ISDKP/strip ]; then
+  sudo cp $ISDK/usr/bin/strip $ISDKP
+fi
+
+make clean
+ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER"
+make HOST_CC="gcc -m32 -std=c99" CROSS="$ISDKP" TARGET_FLAGS="$ISDKF" TARGET=armv7 TARGET_SYS=iOS LUAJIT_A=libsluav7.a
+
+
+make clean
+ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER"
+make HOST_CC="gcc -m32 -std=c99" CROSS="$ISDKP" TARGET_FLAGS="$ISDKF" TARGET=armv7s TARGET_SYS=iOS LUAJIT_A=libsluav7s.a
+
+make clean
+ISDKF="-arch arm64 -isysroot $ISDK/SDKs/$ISDKVER"
+make HOST_CC="gcc -std=c99" CROSS="$ISDKP" TARGET_FLAGS="$ISDKF" TARGET=arm64 TARGET_SYS=iOS LUAJIT_A=libslua64.a
+
+cd src
+lipo libsluav7.a -create libsluav7s.a libslua64.a -output libslua.a
+cp libslua.a ../../../Assets/Plugins/iOS/
+cd ..
+
