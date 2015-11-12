f4a2e40b342cb41f1478e7fc500cde24f4a42d9e
Kelly <chepy.@gmail.com>
2015-11-12 16:15:32 +0800

    MethodFilter

--- Assets/Slua/Editor/CustomExport.cs
+++ Assets/Slua/Editor/CustomExport.cs
@@ -22,56 +22,99 @@
 
 namespace SLua
 {
-	using System.Collections.Generic;
-	using System;
+    using System.Collections.Generic;
+    using System;
 
-	public class CustomExport
-	{
+    public class CustomExport
+    {
 
-		public static void OnAddCustomClass(LuaCodeGen.ExportGenericDelegate add)
-		{
-			add(typeof(System.Func<int>), null);
-			add(typeof(System.Action<int, string>), null);
-			add(typeof(System.Action<int, Dictionary<int, object>>), null);
-			add(typeof(List<int>), "ListInt");
-			add(typeof(Dictionary<int, string>), "DictIntStr");
-			add(typeof(string), "String");
-			// add your custom class here
-			// add( type, typename)
-			// type is what you want to export
-			// typename used for simplify generic type name or rename, like List<int> named to "ListInt", if not a generic type keep typename as null or rename as new type name
-		}
+        public static void OnAddCustomClass(LuaCodeGen.ExportGenericDelegate add)
+        {
+            add(typeof(System.Func<int>), null);
+            add(typeof(System.Action<int, string>), null);
+            add(typeof(System.Action<int, Dictionary<int, object>>), null);
+            add(typeof(List<int>), "ListInt");
+            add(typeof(Dictionary<int, string>), "DictIntStr");
+            add(typeof(string), "String");
+#if KTV_GAMECLIENT
+            add(typeof(KLog), "KLog");
+            add(typeof(KException), "KException");
 
-		public static void OnAddCustomAssembly(ref List<string> list)
-		{
-			// add your custom assembly here
-			// you can build a dll for 3rd library like ngui titled assembly name "NGUI", put it in Assets folder
-			// add it's name into list, slua will generate all exported interface automatically for you
+            add(typeof(KUiTweenFrame), "KUiTweenFrame");
+            add(typeof(KUiFrame), "KUiFrame");
 
-			//list.Add("NGUI");
-		}
+            add(typeof(AnnounceConf), "AnnounceConf");
 
-		public static HashSet<string> OnAddCustomNamespace()
-		{
-			return new HashSet<string>
-			{
-				//"NLuaTest.Mock"
-			};
-		}
+            // NGUI
+            add(typeof(UIButtonColor.State), "UIButtonColorState");
+            add(typeof(UIButtonColor), "UIButtonColor");
+            add(typeof(UIButton), "UIButton");
 
-		// if uselist return a white list, don't check noUseList(black list) again
-		public static void OnGetUseList(out List<string> list)
-		{
-			list = new List<string>
-			{
-				//"UnityEngine.GameObject",
-			};
-		}
+            add(typeof(UICamera), "UICamera");
+            add(typeof(Localization), "Localization");
+            add(typeof(NGUITools), "NGUITools");
+            add(typeof(UIRect), "UIRect");
+            add(typeof(UIWidget), "UIWidget");
+            add(typeof(UIWidgetContainer), "UIWidgetContainer");
+            add(typeof(UILabel), "UILabel");
+            add(typeof(UIToggle), "UIToggle");
+            add(typeof(UIBasicSprite), "UIBasicSprite");
+            add(typeof(UITexture), "UITexture");
+            add(typeof(UISprite), "UISprite");
+            add(typeof(UIProgressBar), "UIProgressBar");
+            add(typeof(UISlider), "UISlider");
+            add(typeof(UIGrid), "UIGrid");
+            add(typeof(UIInput), "UIInput");
+            add(typeof(UIScrollView), "UIScrollView");
+            add(typeof(UITweener), "UITweener");
+            add(typeof(TweenWidth), "TweenWidth");
+            add(typeof(TweenRotation), "TweenRotation");
+            add(typeof(TweenPosition), "TweenPosition");
+            add(typeof(TweenScale), "TweenScale");
+            add(typeof(UICenterOnChild), "UICenterOnChild");
+            add(typeof(UIAtlas), "UIAtlas");
+#endif
+            // add your custom class here
+            // add( type, typename)
+            // type is what you want to export
+            // typename used for simplify generic type name or rename, like List<int> named to "ListInt", if not a generic type keep typename as null or rename as new type name
+        }
 
-		// black list if white list not given
-		public static void OnGetNoUseList(out List<string> list)
-		{
-			list = new List<string>
+        public static void OnAddCustomAssembly(ref List<string> list)
+        {
+            // add your custom assembly here
+            // you can build a dll for 3rd library like ngui titled assembly name "NGUI", put it in Assets folder
+            // add it's name into list, slua will generate all exported interface automatically for you
+
+            //list.Add("NGUI");
+        }
+
+        public static HashSet<string> OnAddCustomNamespace()
+        {
+            return new HashSet<string>
+            {
+                //"NLuaTest.Mock"
+            };
+        }
+
+        // if uselist return a white list, don't check noUseList(black list) again
+        public static void OnGetUseList(out List<string> list)
+        {
+            list = new List<string>
+            {
+                //"UnityEngine.GameObject",
+            };
+        }
+
+        public static List<string> FunctionFilterList = new List<string>()
+        {
+            "UIWidget.showHandles",
+            "UIWidget.showHandlesWithMoveTool",
+        };
+        // black list if white list not given
+        public static void OnGetNoUseList(out List<string> list)
+        {
+            list = new List<string>
         {      
             "HideInInspector",
             "ExecuteInEditMode",
@@ -148,7 +191,7 @@ namespace SLua
             "Advertisements",
             "UnityEditor",
         };
-		}
+        }
 
-	}
+    }
 }
--- Assets/Slua/Editor/LuaCodeGen.cs
+++ Assets/Slua/Editor/LuaCodeGen.cs
@@ -1380,6 +1380,9 @@ namespace SLua
 		
 		bool DontExport(MemberInfo mi)
 		{
+		    var methodString = string.Format("{0}.{1}", mi.DeclaringType, mi.Name);
+		    if (CustomExport.FunctionFilterList.Contains(methodString))
+		        return true;
 			return mi.GetCustomAttributes(typeof(DoNotToLuaAttribute), false).Length > 0;
 		}
 		
