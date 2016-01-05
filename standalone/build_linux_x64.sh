bddc0b285d2faa0605a75d71f58c4299b307b85c
Kelly <chepy.@gmail.com>
2016-01-05 19:36:05 +0800

    DLLMap config

--- standalone/build_linux_x64.sh
+++ standalone/build_linux_x64.sh
@@ -4,11 +4,14 @@ cd $(cd `dirname $0`; pwd)
 cd gmake
 make config=release_linux
 
-echo 'Copying NUnit.dll'
+echo 'Copying NUnit.dll...'
 cp ../packages/NUnit.3.0.1/lib/net20/*.dll ../bin/
 
-echo 'Copying slua.so'
+echo 'Copying slua.so & slua.dll...'
 cp ../../Assets/Plugins/x64/slua.* ../bin/
+
+echo 'Copying dll config...'
+cp ./slua-standalone.dll.config ../bin/
 
 echo 'Success!'
 
