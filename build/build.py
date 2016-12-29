a16cda69c2ec0e96bf5b5a558e288d22b353f28f
lizhou.zhu <lizhou.zhu@happyelements.com>
2016-12-29 17:12:13 +0800

    change work folder before build

--- build/build.py
+++ build/build.py
@@ -5,18 +5,23 @@ import subprocess
 import platform
 import os
 import sys
+
 def build(platform):
     print("[Start Build] Target Platform: " + platform)
+    build_folder = os.path.split(os.path.realpath(__file__))[0]
+    #change folder
+    os.chdir(build_folder)
+
     build_script = ""
     if platform == "windows":
         build_script = "make_win_with_2015_static.bat"
-        subprocess.Popen(["cmd.exe","/C",build_script],shell=True)
     elif platform == "android":
         build_script = "make_android_static.sh"
     elif platform == "ios":
         build_script = "make_ios.sh"
     elif platform == "osx":
         build_script = "make_osx_static.sh"
+    subprocess.check_call(build_script,shell=True)
 
 if __name__ == '__main__':
     length = len(sys.argv)
