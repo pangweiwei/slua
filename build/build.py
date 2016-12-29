d17759b61612c0d6677eace419767dc275a308e1
lizhou.zhu <lizhou.zhu@happyelements.com>
2016-12-29 17:59:47 +0800

    add full cmd

--- build/build.py
+++ build/build.py
@@ -14,21 +14,25 @@ def build(platform):
     os.chdir(build_folder)
 
     build_script = ""
+    build_cmd = ""
     if platform == "windows":
         build_script = "make_win_with_2015_static.bat"
+        build_cmd = ["cmd.exe","/C",build_script]
     elif platform == "android":
         build_script = "make_android_static.sh"
+        build_cmd = ["sh",build_script]
     elif platform == "ios":
         build_script = "make_ios.sh"
+        build_cmd = ["sh",build_script]
     elif platform == "osx":
         build_script = "make_osx_static.sh"
-        
+        build_cmd = ["sh",build_script]
+
     #make script execute able
     #chmod a+x
     st = os.stat(build_script)
     os.chmod(build_script, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-
-    subprocess.check_call(build_script,shell=True)
+    subprocess.check_call(build_cmd,shell=True)
 
 if __name__ == '__main__':
     length = len(sys.argv)
