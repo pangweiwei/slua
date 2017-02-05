7afd586fb8818af988334c7c07f09d38437e8815
siney <sineysan@163.com>
2017-02-05 17:23:07 +0800

    Update LuaSvr.cs
    yield return null

--- Assets/Plugins/Slua_Managed/LuaSvr.cs
+++ Assets/Plugins/Slua_Managed/LuaSvr.cs
@@ -152,7 +152,7 @@ namespace SLua
 				bindProgress = (int)(((float)n / list.Count) * 98.0) + 2;
 				if (_tick!=null && lastProgress != bindProgress && bindProgress % 5 == 0) {
 					tick (bindProgress);
-					yield return bindProgress;
+					yield return null;
 				}
 			}
 
